9cbe286487cd78e3604293ba82d997791f2f3042
nls999 <stephan-000@hotmail.com>
2022-11-15 20:27:20 -0300

    Project setup
    Database connection: h2 for test and MySQL for runtime.

---
+++ src/main/java/com/yougrocery/yougrocery/YouGroceryApplication.java
@@ -0,0 +1,13 @@
+package com.yougrocery.yougrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouGroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouGroceryApplication.class, args);
+	}
+
+}
