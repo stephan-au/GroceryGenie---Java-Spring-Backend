a400c54abb1b543009ca78f20fca2333f5841de4
nls999 <stephan-000@hotmail.com>
2022-11-17 18:43:56 -0300

    Add ResponseBodyMatchers for mvc test
    Makes asserting MvcResult more readable
    
    Source: https://reflectoring.io/spring-boot-web-controller-test/

---
+++ src/test/java/com/yougrocery/yougrocery/controllers/ResponseBodyMatchers.java
@@ -0,0 +1,39 @@
+package com.yougrocery.yougrocery.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResponseBodyMatchers {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    static ResponseBodyMatchers responseBody() {
+        return new ResponseBodyMatchers();
+    }
+
+    <T> ResultMatcher containsObjectAsJson(Object expectedObject,
+                                           Class<T> targetClass) {
+
+        return mvcResult -> {
+            T actualObject = getActualObject(mvcResult, targetClass);
+
+            assertThat(actualObject).usingRecursiveComparison()
+                    .isEqualTo(expectedObject);
+        };
+    }
+
+    private <T> T getActualObject(MvcResult mvcResult, Class<T> targetClass) throws JsonProcessingException, UnsupportedEncodingException {
+        return objectMapper.readValue(
+                getJson(mvcResult),
+                targetClass);
+    }
+
+    private static String getJson(MvcResult mvcResult) throws UnsupportedEncodingException {
+        return mvcResult.getResponse().getContentAsString();
+    }
+}
