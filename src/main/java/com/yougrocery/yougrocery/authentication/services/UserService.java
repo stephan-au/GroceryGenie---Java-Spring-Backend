634cb5f24f5062c56fc07d968f0ba7e9e7c2731a
nls999 <stephan-000@hotmail.com>
2023-02-02 22:07:54 -0300

    Refactor UserDetailsService from bean to implemented in UserService

--- src/main/java/com/yougrocery/yougrocery/authentication/services/UserService.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/services/UserService.java
@@ -4,16 +4,25 @@ import com.yougrocery.yougrocery.authentication.models.User;
 import com.yougrocery.yougrocery.authentication.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
+
     private final UserRepository userRepo;
 
     public User getUser(int id) {
         return userRepo.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
