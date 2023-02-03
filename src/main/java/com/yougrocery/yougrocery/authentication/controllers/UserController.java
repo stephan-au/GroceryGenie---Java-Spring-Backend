58f17ea9d570ca1497bccb73b4e0b92a595d9994
nls999 <stephan-000@hotmail.com>
2023-02-02 21:20:09 -0300

    Remove unnecessary User methods

--- src/main/java/com/yougrocery/yougrocery/authentication/controllers/UserController.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/controllers/UserController.java
@@ -3,14 +3,12 @@ package com.yougrocery.yougrocery.authentication.controllers;
 
 import com.yougrocery.yougrocery.authentication.models.User;
 import com.yougrocery.yougrocery.authentication.services.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -18,24 +16,8 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok().body(userService.getAll());
-    }
-
     @GetMapping("/get/{id}")
     public ResponseEntity<User> getUser(@PathVariable("id") int id) {
         return ResponseEntity.ok().body(userService.getUser(id));
-    }
-
-    @PostMapping("/user/save")
-    public ResponseEntity<User> saveUser(@RequestBody @Valid User user) {
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
-        return ResponseEntity.created(uri).body(userService.saveUser(user));
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Boolean> deleteUser(@PathVariable("id") int id) {
-        return ResponseEntity.ok(userService.delete(id));
     }
 }
--- src/main/java/com/yougrocery/yougrocery/authentication/services/UserService.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/services/UserService.java
@@ -4,51 +4,16 @@ import com.yougrocery.yougrocery.authentication.models.User;
 import com.yougrocery.yougrocery.authentication.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static java.lang.Boolean.TRUE;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
-public class UserService implements UserDetailsService {
+public class UserService {
     private final UserRepository userRepo;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = getUserByEmail(email);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found in the database");
-        }
-
-        return user;
-    }
-
-    public User saveUser(User user) {
-        return userRepo.save(user);
-    }
 
     public User getUser(int id) {
         return userRepo.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    }
-
-    public User getUserByEmail(String email) {
-        return userRepo.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    }
-
-    public List<User> getAll() {
-        return userRepo.findAll();
-    }
-
-    public Boolean delete(int id) {
-        userRepo.deleteById(id);
-        return TRUE;
     }
 }
