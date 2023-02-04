f82bc342e5fc5fb30d4d217c5968853bf3808e6b
nls999 <stephan-000@hotmail.com>
2023-02-03 22:43:28 -0300

    Make JwtAuthenticationFilter protected

--- src/main/java/com/yougrocery/yougrocery/authentication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/config/JwtAuthenticationFilter.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
