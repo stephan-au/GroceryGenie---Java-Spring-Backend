b9e6768682d7d2b93ee65ac212b7e2af93784247
nls999 <stephan-000@hotmail.com>
2023-02-03 21:06:05 -0300

    Small refactor endpoint string

--- src/main/java/com/yougrocery/yougrocery/grocerylist/controllers/GroceryItemController.java
+++ src/main/java/com/yougrocery/yougrocery/grocerylist/controllers/GroceryItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/product_on_grocerylist")
+@RequestMapping("/api/v1/grocery_item")
 @RequiredArgsConstructor
 public class GroceryItemController {
 
