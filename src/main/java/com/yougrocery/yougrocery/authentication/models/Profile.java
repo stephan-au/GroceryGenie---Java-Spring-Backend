d601f6c4fd8c08830a02cc8bc4d8c2fb43d488f5
nls999 <stephan-000@hotmail.com>
2023-02-09 15:47:37 -0300

    Change profile ID to int and add Validations

--- src/main/java/com/yougrocery/yougrocery/authentication/models/Profile.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/models/Profile.java
@@ -1,6 +1,7 @@
 package com.yougrocery.yougrocery.authentication.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.UUID;
@@ -14,11 +15,13 @@ import java.util.UUID;
 @Table(name = "profile")
 public class Profile {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profile_generator")
+    @SequenceGenerator(name = "profile_generator", sequenceName = "seq_profile", allocationSize = 1)
     @Column(name = "id", nullable = false)
-    private UUID id;
+    private int id;
 
-    @Column(name = "display_name", length = 100)
+    @Column(name = "display_name", length = 101)
+    @Size(max = 101)
     private String displayName;
 
     @Column(name = "profile_picture_url")
