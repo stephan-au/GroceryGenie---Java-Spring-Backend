17203d65ced46aace0c75a98bb242a5113afe88e
nls999 <stephan-000@hotmail.com>
2023-02-09 15:48:17 -0300

    Refactor EmailAuthentication now creates userProfile too

--- src/main/java/com/yougrocery/yougrocery/authentication/dtos/EmailRegisterRequestDTO.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/dtos/EmailRegisterRequestDTO.java
@@ -1,8 +1,24 @@
 package com.yougrocery.yougrocery.authentication.dtos;
 
+import com.yougrocery.yougrocery.authentication.models.Profile;
+import com.yougrocery.yougrocery.authentication.models.Role;
+import com.yougrocery.yougrocery.authentication.models.User;
+
 public record EmailRegisterRequestDTO(
         String firstName,
         String lastName,
         String email,
         String password) {
+
+    public User convertToUser() {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .role(Role.USER)
+                .userProfile(Profile.builder()
+                        .displayName(String
+                                .format("%s %s", firstName, lastName))
+                        .build())
+                .build();
+    }
 }
--- src/main/java/com/yougrocery/yougrocery/authentication/services/EmailAuthenticationService.java
+++ src/main/java/com/yougrocery/yougrocery/authentication/services/EmailAuthenticationService.java
@@ -1,35 +1,26 @@
 package com.yougrocery.yougrocery.authentication.services;
 
-import com.yougrocery.yougrocery.authentication.dtos.EmailAuthenticationRequestDTO;
 import com.yougrocery.yougrocery.authentication.dtos.AuthenticationResponseDTO;
+import com.yougrocery.yougrocery.authentication.dtos.EmailAuthenticationRequestDTO;
 import com.yougrocery.yougrocery.authentication.dtos.EmailRegisterRequestDTO;
-import com.yougrocery.yougrocery.authentication.models.Role;
 import com.yougrocery.yougrocery.authentication.models.User;
-import com.yougrocery.yougrocery.authentication.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailAuthenticationService {
-    private final UserRepository repository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponseDTO register(EmailRegisterRequestDTO request) {
-        var user = User.builder()
-                .firstName(request.firstName())
-                .lastName(request.lastName())
-                .email(request.email())
-                .password(passwordEncoder.encode(request.password()))
-                .role(Role.USER)
-                .build();
+        User user = request.convertToUser();
 
-        repository.save(user);
+        userService.registerUser(user);
 
         return new AuthenticationResponseDTO(generateToken(user));
     }
@@ -41,13 +32,12 @@ public class EmailAuthenticationService {
                         request.password()
                 )
         );
-        var user = repository.findByEmail(request.email())
-                .orElseThrow();
+        UserDetails user = userService.loadUserByUsername(request.email());
 
         return new AuthenticationResponseDTO(generateToken(user));
     }
 
-    private String generateToken(User user) {
+    private String generateToken(UserDetails user) {
         return jwtService.generateToken(user);
     }
 }
